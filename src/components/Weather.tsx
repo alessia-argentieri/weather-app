af6834795ad01afd11e6804b18fa6b34b30f0e43
alex arg <alex.arg.ax@gmail.com>
2024-04-16 19:30:32 +0100

    add weather-box with API response: city and country, weather description, rounded temperatures, humidity and wind speed, and weather icon

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -55,23 +55,54 @@ const Weather: FC = () => {
   };
 
   return (
-    <div>
-      <header className="header">
-        <h1>Weather App</h1>
-      </header>
-      <form className="search-box" onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="text"
-          placeholder="Enter city"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-        ></input>
-        <button className="btn" type="submit">
-          Search
-        </button>
-      </form>
-    </div>
+    <>
+      <div>
+        <header className="header">
+          <h1>Weather App</h1>
+        </header>
+        <form className="search-box" onSubmit={handleSubmit}>
+          <input
+            className="input"
+            type="text"
+            placeholder="Enter city"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+          ></input>
+          <button className="btn" type="submit">
+            Search
+          </button>
+        </form>
+        {data !== undefined && (
+          <div className="container">
+            <div className="weather-box">
+              <h2 className="item">
+                {data.name}, {data.sys.country}
+              </h2>
+              <h2 className="item description">
+                {data.weather[0].description}
+              </h2>
+              <h2 className="item">{Math.round(data.main.temp)} °C</h2>
+              <img
+                className="icon"
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+              ></img>
+
+              <h2 className="item">Max: {Math.round(data.main.temp_max)} °C</h2>
+              <h2 className="item">Min: {Math.round(data.main.temp_min)} °C</h2>
+              <h2 className="item">
+                Perceived: {Math.round(data.main.feels_like)} °C
+              </h2>
+              <h2 className="item">
+                Humidity: {Math.round(data.main.humidity)}%
+              </h2>
+              <h2 className="item">
+                Wind speed: {Math.round(data.wind.speed * 3.6)} km/h
+              </h2>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
