e6fe59b3251c3e63eb8365e170802cb512f79fdb
alex arg <alex.arg.ax@gmail.com>
2024-04-16 19:23:46 +0100

    add API call, create env file with API key, add setData functionality and handleSubmit function

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,13 +1,65 @@
-import { FC, useState } from "react";
+import { FC, useState, FormEventHandler } from "react";
+
+type Api = {
+  key: string;
+  url: string;
+};
+
+type Response = {
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  name: string;
+};
+
+const api: Api = {
+  key: import.meta.env.VITE_API_KEY,
+  url: "https://api.openweathermap.org/data/2.5/",
+};
 
 const Weather: FC = () => {
   const [search, setSearch] = useState("");
+  const [data, setData] = useState<Response>();
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    fetch(`${api.url}weather?q=${search}&units=metric&appid=${api.key}`)
+      .then((response) => response.json())
+      .then((data: Response) => {
+        setData(data);
+        setSearch("");
+      })
+      .catch((error) => console.log(`Error ${error}`));
+  };
+
   return (
     <div>
       <header className="header">
         <h1>Weather App</h1>
       </header>
-      <form className="search-box">
+      <form className="search-box" onSubmit={handleSubmit}>
         <input
           className="input"
           type="text"
